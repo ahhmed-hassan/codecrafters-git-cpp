6a570fec967ee067388d4ce2811ddbca6480a924
Ahmed Hassan <ahmedhosam.1591@gmail.com>
2025-05-06 22:17:10 +0200

    h

--- src/Server.cpp
+++ src/Server.cpp
@@ -4,9 +4,25 @@
 #include <string>
 #include "gitt/commands.h"
 
+//#define DEBUG
+#ifndef DEBUG
+int main(int argc, char *argv[]){
+#else
+#include<sstream>
+int main() {
+    std::string input;
+    std::getline(std::cin, input);
 
-int main(int argc, char *argv[])
-{
+    std::istringstream iss(input);
+    std::vector<std::string> argv((std::istream_iterator<std::string>(iss)),
+        std::istream_iterator<std::string>());
+    argv.insert(argv.begin(), "notImportant");
+    size_t argc = argv.size();
+
+#endif // DEBUG
+
+
+
     // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
@@ -27,7 +43,8 @@ int main(int argc, char *argv[])
          return commands::init_command();
      }
      else if (command == "cat-file") {
-         
+         std::string option = argv[2]; 
+         std::string arg = argv[3]; 
          return commands::cat_command(argv[2], argv[3]);
      }
      else {
