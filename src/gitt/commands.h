81ae76c3246815f9dc5df59ff5e980b29345ecca
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-05-13 17:00:34 +0200

    Adding modes constants

--- src/gitt/commands.h
+++ src/gitt/commands.h
@@ -10,6 +10,14 @@ namespace commands
 		std::filesystem::path const objectsDir = gitDir / "objects";
 		std::filesystem::path const refsDir = gitDir / "refs";
 		std::filesystem::path const head = gitDir / "HEAD"; 
+
+		namespace gitConsts 
+		{
+			std::string const regularFile{ "10644" }; 
+			std::string const excutableFile{ "100755" }; 
+			std::string const symbolink{ "12000" }; 
+			std::string const directory{ "40000" }; 
+		}
 	}
 	int init_command();
 	int cat_command(std::string option, std::string args);
