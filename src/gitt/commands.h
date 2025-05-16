d1ce6653d04ddd6379993742cecda5cdc5ed71de
Ahmed Hassan <ahmedhosam.1591@gmail.com>
2025-05-16 18:08:12 +0200

    First attempt for write-tree

--- src/gitt/commands.h
+++ src/gitt/commands.h
@@ -4,6 +4,7 @@
 #include <filesystem>
 #include <vector>
 #include <map>
+#include <expected>
 namespace commands
 {
 	namespace constants
@@ -35,11 +36,18 @@ namespace commands
 		std::string perm_{}; 
 		std::string name_{}; 
 		std::string shaHash_{}; 
+		Tree() = default; 
+		Tree(std::string_view perm, std::string_view name, std::string_view hash);
+		Tree(std::filesystem::directory_entry const& de, std::string const& hash); 
 	};
 	int init_command();
 	int cat_command(std::string option, std::string args);
-	int hash_command(std::filesystem::path const& path, bool wrtiteThebject);
+	int hash_command(std::filesystem::path const& path, bool wrtiteThebject, bool print = false);
+	
+	std::expected<std::string, std::string> create_hash_and_give_sha(std::filesystem::path const& path, bool writeTheObject);
+	
 	int ls_tree_command(std::string args, bool namesOnly); 
+	int write_tree_command();
 	namespace utilities
 	{
 		std::vector<Tree> parse_trees(std::string const& treeHash = "3d59ede50c909c5d64b8cdb3cbe31992be17e447");
