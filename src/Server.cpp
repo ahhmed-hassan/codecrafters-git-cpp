4ee2a4c312e741a1bd3245bf00916fc6bb8c0d44
Ahmed Hassan <ahmedhosam.1591@gmail.com>
2025-05-17 19:55:45 +0200

    Deactiating debug modus

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 #include<sstream>
 #include "gitt/commands.h"
 
-#define DEBUG
+//#define DEBUG
 #ifndef DEBUG
 int main(int argc, char *argv[]){
     std::vector<std::string> args(argv, argv + argc);
