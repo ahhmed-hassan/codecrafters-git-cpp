8ceaf28cebc5649344eec7b268ec8999b30c8c0b
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2025-05-26 16:25:16 +0200

    Blueprint for fetching the packfile

--- src/Server.cpp
+++ src/Server.cpp
@@ -12,116 +12,119 @@
 template <class Val, class Err>
 int convert_expected(const std::expected<Val, Err>& e, bool print)
 {
-    if (e)
-    {
-        if(print) std::cout << e.value() << "\n";
-        return EXIT_SUCCESS; 
-    }
-    else
-    {
-        std::cerr << e.value() << "\n";
-        return EXIT_FAILURE;
-    }
+	if (e)
+	{
+		if (print) std::cout << e.value() << "\n";
+		return EXIT_SUCCESS;
+	}
+	else
+	{
+		std::cerr << e.value() << "\n";
+		return EXIT_FAILURE;
+	}
 };
 
 //#define DEBUG
 #ifndef DEBUG
-int main(int argc, char *argv[]){
-    std::vector<std::string> args(argv, argv + argc);
-    auto h = clone::get_info_refs();
-    std::cout << h<<"\n";
-    using namespace std::string_view_literals;
-    std::cout<< "\n\n" <<clone::get_head_sha() << "\n";
-   
-    return 0; 
+int main(int argc, char* argv[]) {
+	std::vector<std::string> args(argv, argv + argc);
+	auto h = clone::get_info_refs();
+	auto const url = "https://github.com/git/git";
+	//std::cout << h<<"\n";https
+	auto g = clone::parse_info_refs(clone::get_info_refs());
+	using namespace std::string_view_literals;
+	auto shaHead = clone::get_head_sha(url);
+	//std::cout<< "\n\n" <<clone::get_head_sha() << "\n";
+	std::cout << clone::fetch_packfile(url, shaHead);
+	return 0;
 
 #else
 #include<sstream>
 int main() {
-    std::string input;
-    std::getline(std::cin, input);
+	std::string input;
+	std::getline(std::cin, input);
 
-    std::istringstream iss(input);
-    std::vector<std::string> args((std::istream_iterator<std::string>(iss)),
-        std::istream_iterator<std::string>());
-    args.insert(args.begin(), "notImportant");
-    size_t argc = args.size();
+	std::istringstream iss(input);
+	std::vector<std::string> args((std::istream_iterator<std::string>(iss)),
+		std::istream_iterator<std::string>());
+	args.insert(args.begin(), "notImportant");
+	size_t argc = args.size();
 
 #endif // DEBUG
 
 
 
 
-    // Flush after every std::cout / std::cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
+	// Flush after every std::cout / std::cerr
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here!\n";
-
-    
-     
-     if (argc < 2) {
-         std::cerr << "No command provided.\n";
-         return EXIT_FAILURE;
-     }
-    
-     std::string command = args[1];
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	std::cerr << "Logs from your program will appear here!\n";
 
 
 
-     if (command == "init") {
-         return commands::init();
-     }
-     else if (command == "cat-file") {
-         std::string option = args[2]; 
-         std::string arg = args[3]; 
-         return commands::cat(args[2], args[3]);
-     }
-     else if (command == "hash-object")
-     {
-         
-         std::string arg = args.back(); 
-         bool writeOption = std::ranges::find(args, "-w")!= args.end();
-         return convert_expected(commands::hash(arg, writeOption, false), true);
-     }
-     else if (command == "ls-tree")
-     {
-         std::string arg = args.back(); 
-         bool namesOnly = std::ranges::find(args, "--name-only") != args.end(); 
-         return commands::ls_tree(arg, namesOnly); 
-     }
-     else if (command == "write-tree")
-     {
-         return commands::write_tree();
-     }
-     else if (command == "commit-tree")
-     {
-         std::string treeHash = *std::next(std::ranges::find(args,"commit-tree"));
-         std::optional<std::string> parentTreeHash{ std::nullopt };
-         auto parentOptionIt = std::ranges::find(args, "-p");
-         if ( parentOptionIt != args.end())
-             parentTreeHash = *std::next(parentOptionIt);
+	if (argc < 2) {
+		std::cerr << "No command provided.\n";
+		return EXIT_FAILURE;
+	}
 
-         std::optional<std::string> msg{ std::nullopt };
-         if (auto msgOptionIt = std::ranges::find(args, "-m"); msgOptionIt != args.end())
-         {
-             auto endOfQuotedStringIT = std::ranges::find_if(args, [](std::string_view s) {return s.ends_with('\"'); });
-             auto test = std::ranges::join_with_view(std::ranges::subrange(std::next(msgOptionIt), endOfQuotedStringIT), ' ');
-             auto reconstructedQuotedMsg = std::ranges::fold_left(test, std::string{}, std::plus());
-        
-             msg = reconstructedQuotedMsg.starts_with('\"') and reconstructedQuotedMsg.ends_with('\"') ?
-                 reconstructedQuotedMsg.substr(1, reconstructedQuotedMsg.size() - 2) :
-                 reconstructedQuotedMsg;
-         }
+	std::string command = args[1];
 
-             
-         return commands::commmit(treeHash, parentTreeHash, msg); 
-     }
-     else {
-         std::cerr << "Unknown command " << command << '\n';
-         return EXIT_FAILURE;
-     }
-    
-     //return EXIT_SUCCESS;
+
+
+	if (command == "init") {
+		return commands::init();
+	}
+	else if (command == "cat-file") {
+		std::string option = args[2];
+		std::string arg = args[3];
+		return commands::cat(args[2], args[3]);
+	}
+	else if (command == "hash-object")
+	{
+
+		std::string arg = args.back();
+		bool writeOption = std::ranges::find(args, "-w") != args.end();
+		return convert_expected(commands::hash(arg, writeOption, false), true);
+	}
+	else if (command == "ls-tree")
+	{
+		std::string arg = args.back();
+		bool namesOnly = std::ranges::find(args, "--name-only") != args.end();
+		return commands::ls_tree(arg, namesOnly);
+	}
+	else if (command == "write-tree")
+	{
+		return commands::write_tree();
+	}
+	else if (command == "commit-tree")
+	{
+		std::string treeHash = *std::next(std::ranges::find(args, "commit-tree"));
+		std::optional<std::string> parentTreeHash{ std::nullopt };
+		auto parentOptionIt = std::ranges::find(args, "-p");
+		if (parentOptionIt != args.end())
+			parentTreeHash = *std::next(parentOptionIt);
+
+		std::optional<std::string> msg{ std::nullopt };
+		if (auto msgOptionIt = std::ranges::find(args, "-m"); msgOptionIt != args.end())
+		{
+			auto endOfQuotedStringIT = std::ranges::find_if(args, [](std::string_view s) {return s.ends_with('\"'); });
+			auto test = std::ranges::join_with_view(std::ranges::subrange(std::next(msgOptionIt), endOfQuotedStringIT), ' ');
+			auto reconstructedQuotedMsg = std::ranges::fold_left(test, std::string{}, std::plus());
+
+			msg = reconstructedQuotedMsg.starts_with('\"') and reconstructedQuotedMsg.ends_with('\"') ?
+				reconstructedQuotedMsg.substr(1, reconstructedQuotedMsg.size() - 2) :
+				reconstructedQuotedMsg;
+		}
+
+
+		return commands::commmit(treeHash, parentTreeHash, msg);
+	}
+	else {
+		std::cerr << "Unknown command " << command << '\n';
+		return EXIT_FAILURE;
+	}
+
+	//return EXIT_SUCCESS;
 }
--- src/gitt/clone.cpp
+++ src/gitt/clone.cpp
@@ -4,80 +4,120 @@
 #include <cpr/cpr.h>
 #include "clone.h"
 
-std::string clone::get_info_refs(const std::string& url)
+namespace clone
 {
-    std::string full_url = std::format("{}/info/refs?service=git-upload-pack", url);
-    cpr::Response r = cpr::Get(cpr::Url{ full_url });
+    std::string clone::get_info_refs(const std::string& url)
+    {
+        std::string full_url = std::format("{}/info/refs?service=git-upload-pack", url);
+        cpr::Response r = cpr::Get(cpr::Url{ full_url });
 
-    if (r.status_code != 200) {
-        throw std::runtime_error("HTTP request failed");
+        if (r.status_code != 200) {
+            throw std::runtime_error("HTTP request failed");
+        }
+        return r.text;
     }
-    return r.text;
-}
 
-std::vector<clone::Ref> clone::parse_info_refs(std::string const& getResponse)
-{
-    std::vector<Ref> refs;
-    std::istringstream iss(getResponse);
-    std::string line;
+    std::vector<clone::Ref> clone::parse_info_refs(std::string const& getResponse)
+    {
+        std::string const header = "0000";
+        std::vector<Ref> refs;
+        std::istringstream iss(getResponse);
+        std::string line;
+        //Consume the first line
+        std::getline(iss, line);
 
-    while (std::getline(iss, line)) {
-        // Skip flush packets and service header
-        if (line.size() < 4/* || line.substr(0, 4) == "0000"*/) continue;
-        if (line.find("# service=") != std::string::npos) continue;
+        while (std::getline(iss, line)) {
+            // Skip flush packets and service header
+            if (line == header) continue;
+            //if (line.find("# service=") != std::string::npos) continue;
 
-        // Extract pkt-line length (first 4 chars as hex)
-        uint32_t length;
-        std::stringstream len_ss;
-        len_ss << std::hex << line.substr(0, 4);
-        len_ss >> length;
+            // Extract pkt-line length (first 4 chars as hex)
+            uint32_t length;
+            std::stringstream len_ss;
+            len_ss << std::hex << line.substr(0, 4);
+            len_ss >> length;
 
-        if (length == 0)
-        {
-            if (line.size() > 4)
+            if (length == 0)
             {
-                line = line.substr(8);
+                if (line.size() > 4)
+                {
+                    line = line.substr(8);
+                }
+                else
+                    continue;
             }
             else
-                continue;
-        }
-        else
-            line = line.substr(4, length - 4);
+                line = line.substr(4, length - 4);
 
-        // Extract actual data (after 4-byte header)
-        //std::string data = line.substr(4, length - 4);
 
-        // Parse into GitRef
-        GitRef ref;
-        size_t first_space = line.find(' ');
-        size_t null_pos = line.find('\0');
 
-        ref.object_id = line.substr(0, first_space);
-        ref.name = line.substr(first_space + 1,
-            (null_pos != std::string::npos) ? null_pos - first_space - 1 : std::string::npos);
+            // Parse into GitRef
+            GitRef ref;
+            size_t first_space = line.find(' ');
+            size_t null_pos = line.find('\0');
 
-        // Parse capabilities (only for HEAD)
-        if (/*ref.name == "HEAD" &&*/ null_pos != std::string::npos || line.contains("HEAD")) {
-            HeadRef headRef{ .ref = ref };
-            std::string caps_str = line.substr(null_pos + 1);
-            std::istringstream caps_iss(caps_str);
-            std::string cap;
-            while (std::getline(caps_iss, cap, ' ')) {
-                headRef.capabilities.push_back(cap);
+            ref.object_id = line.substr(0, first_space);
+            ref.name = line.substr(first_space + 1,
+                (null_pos != std::string::npos) ? null_pos - first_space - 1 : std::string::npos);
+
+            // Parse capabilities (only for HEAD)
+            if (/*ref.name == "HEAD" &&*/ null_pos != std::string::npos || line.contains("HEAD")) {
+                HeadRef headRef{ .ref = ref };
+                std::string caps_str = line.substr(null_pos + 1);
+                std::istringstream caps_iss(caps_str);
+                std::string cap;
+                while (std::getline(caps_iss, cap, ' ')) {
+                    headRef.capabilities.push_back(cap);
+                }
+                refs.push_back(headRef); continue;
             }
-            refs.push_back(headRef); continue; 
+
+            refs.push_back(ref);
         }
 
-        refs.push_back(ref);
+        return refs;
     }
 
-    return refs;
-}
+    std::string clone::get_head_sha(std::string const& url)
+    {
+        auto response = get_info_refs(url);
+        auto refs = clone::parse_info_refs(response);
+        auto headRef = std::get<HeadRef>(*refs.begin());
+        return headRef.ref.object_id;
+    }
 
-std::string clone::get_head_sha(std::string const& url)
-{
-    auto response = get_info_refs(url); 
-    auto refs = clone::parse_info_refs(response); 
-    auto headRef = std::get<HeadRef>(*refs.begin());
-    return headRef.ref.object_id;
+    std::string build_negotiation_body(const std::string& head_sha) {
+        // Format want/done commands with pkt-line encoding
+        std::string want = std::format("want {}\n", head_sha);
+        std::string done = "done\n";
+
+        // Convert to pkt-line (4-byte hex length + data)
+        auto format_pkt_line = [](const std::string& data) {
+            return std::format("{:04x}{}", data.size() + 4, data);
+            };
+
+        return format_pkt_line(want) + format_pkt_line(done) + "0000"; // Flush packet
+    }
+
+    std::string fetch_packfile(const std::string& url,  std::string head_sha) {
+        std::string upload_pack_url = std::format("{}/git-upload-pack", url);
+        std::string body = build_negotiation_body(head_sha);
+
+        cpr::Response r = cpr::Post(
+            cpr::Url{ upload_pack_url },
+            cpr::Body{ body },
+            cpr::Header{
+                {"Content-Type", "application/x-git-upload-pack-request"},
+                {"User-Agent", "my-git-client/1.0"}
+            } 
+            
+        );
+
+        if (r.status_code != 200) {
+            throw std::runtime_error("Failed to fetch packfile");
+        }
+        return r.text;
+      
+    }
+
 }
