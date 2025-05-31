fa88898d15b52c8f799a82fc8428a1a8eab4781e
Ahmed Hassan <ahmedhosam.1591@gmail.com>
2025-05-31 19:58:44 +0200

    Refactoring

--- src/gitt/clone.h
+++ src/gitt/clone.h
@@ -3,7 +3,8 @@
 #include <vector>
 #include <expected>  
 #include <variant>
-#include <cpr/cpr.h>
+
+#include "infrastructure.h"
 namespace clone
 {
     template<class... Ts> struct overload : Ts... { using Ts::operator()...; };
@@ -23,9 +24,8 @@ namespace clone
         [](const GitRef& gitref) {return std::make_tuple(gitref.name, gitref.object_id); },
         [](HeadRef&& head) {return std::make_tuple(head.ref.name, head.ref.object_id, head.capabilities); }
     };*/
-    std::string get_info_refs(const std::string& url= "https://github.com/i27ae15/git.git");
-    std::vector<Ref> parse_info_refs(std::string const& getResponse);
-    std::string get_head_sha(std::string const& url = "https://github.com/i27ae15/git.git");
+    std::string get_refs_info(const std::string& url= "https://github.com/i27ae15/git.git");
+    std::vector<Ref> parse_refs_info(std::string const& getResponse);
     HeadRef get_head(std::string const& url); 
     std::string build_negotiation_body(HeadRef head);
     std::string fetch_packfile(const std::string& url,  HeadRef head); 
