ff69a01c97bbf6305648402639a49db9010098d1
Ahmed Hassan <ahmedhosam.1591@gmail.com>
2025-06-15 15:46:09 +0200

    Refactoring: Moving the utilities to a seperate file as they are used by the inner clone namesapce now1

---
+++ src/gitt/utilities.cpp
@@ -0,0 +1,63 @@
+#include "utilities.h"
+#include<filesystem>
+#include<iostream>
+#include <fstream>
+#include "zstr.hpp"
+#include <zlib.h>
+#include<algorithm>
+#include<ranges>
+#include <functional>
+#include <numeric>
+#include <chrono>
+#include "constants.h"
+
+#include <openssl/sha.h>
+
+#include <format>
+namespace commands
+{
+	namespace fs = std::filesystem; 
+	namespace utilities
+	{
+		std::string sha1_hash(const std::string& content) {
+			unsigned char hash[SHA_DIGEST_LENGTH];
+			SHA1(reinterpret_cast<const unsigned char*>(content.data()), content.size(), hash);
+
+			std::stringstream ss;
+			for (int i = 0; i < SHA_DIGEST_LENGTH; ++i) {
+				ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
+			}
+			return ss.str();
+
+		}
+
+		std::string zlib_compressed_str(std::string const& input)
+		{
+			auto compressedSize = compressBound(static_cast<uLong>(input.size()));
+			std::string compressed{}; compressed.resize(compressedSize);
+			compress(reinterpret_cast<Bytef*>(&compressed[0]), &compressedSize, reinterpret_cast<Bytef const*>(input.data()), static_cast<uLong>(input.size()));
+			compressed.resize(compressedSize);
+			return compressed;
+		}
+
+		std::expected<std::string, std::string> hash_and_save(std::string const& toHash, bool save)
+		{
+			auto objectHash = utilities::sha1_hash(toHash);
+			auto objectDirPath = constants::objectsDir / objectHash.substr(0, 2);
+			fs::create_directories(objectDirPath);
+			const auto filePath = objectDirPath / objectHash.substr(2);
+			auto compressed = utilities::zlib_compressed_str(toHash);
+
+			if (!save)
+				return objectHash;
+
+			std::ofstream outputHashStream(filePath);
+			if (!outputHashStream)
+				return std::unexpected(std::format("Cannot open the file: \t {}", filePath.string()));
+			outputHashStream << compressed;
+			outputHashStream.close();
+			return objectHash;
+
+		}
+	}
+}
